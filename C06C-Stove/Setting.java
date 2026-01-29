e1b2a9593f979ce2b51a9292e4f7cd369caa417f
Your New Name <vann7568@gmail.com>
2026-01-29 13:42:00 -0700

    added code for setting

--- C06C-Stove/Setting.java
+++ C06C-Stove/Setting.java
@@ -1,3 +1,13 @@
 public enum Setting {
+    OFF("---"), LOW("--+"), MEDIUM("-++"),HIGH("+++");
     
+    private final String intensity;
+
+    private Setting(String intensity){
+        this.intensity = intensity;
+    }
+    @Override
+    public String toString(){
+        
+    }
 }
