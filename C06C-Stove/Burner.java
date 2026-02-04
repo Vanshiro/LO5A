f4446ee5d56ae3fec9c6a7064b2765dfbd65dcd9
Your New Name <vann7568@gmail.com>
2026-02-04 14:19:16 -0700

    Comments

--- C06C-Stove/Burner.java
+++ C06C-Stove/Burner.java
@@ -2,6 +2,7 @@ public class Burner {
     public enum Temperature{
         COLD, WARM, HOT, BLAZING;
 
+        //Next function to get the next value in enum
         public Temperature next() {
         if (this.ordinal() == values().length - 1) {
             return this;
@@ -9,21 +10,24 @@ public class Burner {
         return values()[this.ordinal() + 1];
         }
     }
-
+    
     private Temperature myTemperature;
     private Setting mySetting;
     private int timer;
     final static int TIME_DURATION = 2;
 
+    //Constructor to set burner temp and stove setting
     public Burner(){
         myTemperature = Temperature.COLD;
         mySetting = Setting.OFF;
     }
 
+    //Getter to get temperature
     public Temperature getTemperature(){
         return myTemperature;
     }
 
+    //Increase setting by 1 and checks if it cannot be increased further
     public void plusButton(){
         timer = TIME_DURATION;
         switch(mySetting){
@@ -42,6 +46,7 @@ public class Burner {
         }  
     }
 
+    //Decrease setting by 1 and checks if it cannot be decreased further
     public void minusButton(){
         timer = TIME_DURATION;
         switch(mySetting){
@@ -59,6 +64,7 @@ public class Burner {
         }
     }
 
+    //Checks if Temperature enum index is still less than setting, to determine if temperature can still rise.
     public void updateTemperature(){
         if(timer == 0){
             if(myTemperature.ordinal() < mySetting.ordinal()-1){
@@ -73,6 +79,7 @@ public class Burner {
         }
     }
 
+    //Display current stove intensity 
     public void display(){
         String stoveIntensity = mySetting.toString();
         switch(mySetting){
